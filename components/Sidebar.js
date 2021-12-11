0a82685411a1d8ae77ec62ee7a5b58b16352aa24
Varun <varungupta2015135@gmail.com>
2021-12-11 17:58:54 +0530

    Fixed sidebar bug: hiding playlist names

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,9 +3,9 @@ import {
   SearchIcon,
   LibraryIcon,
   PlusCircleIcon,
-  HeartIcon,
   RssIcon,
 } from "@heroicons/react/outline";
+import { HeartIcon } from "@heroicons/react/solid";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { playlistIdState } from "../atoms/playlistAtom";
@@ -25,7 +25,7 @@ function Sidebar() {
   }, [spotifyAPI]);
 
   return (
-    <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
+    <div className="flex-grow text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
@@ -46,24 +46,25 @@ function Sidebar() {
           <p>Create Playlist</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5" />
+          <HeartIcon className="h-5 w-5 text-blue-500" />
           <p>Liked Songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
+          <RssIcon className="h-5 w-5 text-green-500" />
           <p>Your Episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
-
-        {playlists.map((playlist) => (
-          <p
-            key={playlist.id}
-            className="cursor-pointer hover:text-white"
-            onClick={() => setPlaylistId(playlist.id)}
-          >
-            {playlist.name}
-          </p>
-        ))}
+        <div className="space-y-4 pb-24">
+          {playlists.map((playlist) => (
+            <p
+              key={playlist.id}
+              className="cursor-pointer hover:text-white"
+              onClick={() => setPlaylistId(playlist.id)}
+            >
+              {playlist.name}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
